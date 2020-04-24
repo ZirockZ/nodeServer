016484b4f6b8376bf64283d286c932ec7f978bb3
Andrey <karev-2014@mail.ru>
2020-04-24 15:53:39 +0300

    Added socket.io

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http');
+var socket = require('socket.io');
 const app = require('./app.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const server = http.createServer(app);
-
 server.listen(port);
+
+const io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('Socket connected successfully')
+
+    socket.on('new message', (action) => {
+        io.sockets.emit('new message', action)
+    })
+});
